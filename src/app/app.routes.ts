ca508a408dbacf2b3b72aff8901f0c49f3d3b238
alia <aliawahida@icloud.com>
2025-11-11 17:44:26 +0800

    Prepare for Vercel deployment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full',
-  },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   {
     path: 'login',
     loadComponent: () => import('./pages/login/login.page').then(m => m.LoginPage)
